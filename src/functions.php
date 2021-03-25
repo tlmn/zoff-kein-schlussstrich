0d2fcbb5e26b29cd12e6c38d66c04c62dbce0393
Tilman Miraß <mail@tlmn.io>
2021-03-25 16:56:20 +0100

    import and run functions

--- src/functions.php
+++ src/functions.php
@@ -1,15 +1,21 @@
 <?php
 
 include('functions/func-style.php');
+include('functions/func-acf.php');
 include('functions/func-cpt.php');
 include('functions/func-menus.php');
 include('functions/func-scripts.php');
 include('functions/func-blocks.php');
 include('functions/func-admin.php');
+include('functions/func-taxonomies.php');
+
+add_action('init', 'custom_taxonomy_labels', 0);
+add_action('init', 'cpt_event', 0);
+add_action('init', 'register_menus', 0);
 
 add_action('wp_enqueue_scripts', 'js_scripts_jquery', 0);
 add_action('wp_enqueue_scripts', 'js_scripts_custom', 1);
-add_action('admin_head', 'gb_gutenberg_admin_styles');
-add_action('acf/init', 'acf_init_blocks');
-add_action( 'init', 'cpt_event' );
 
+add_filter('acf/settings/remove_wp_meta_box', '__return_false');
+add_action('acf/init', 'acf_init_fields');
+add_action('acf/init', 'acf_blocks');
