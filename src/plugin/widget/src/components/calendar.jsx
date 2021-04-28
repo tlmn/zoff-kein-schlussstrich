52dd9ba9c6b1afff780b6e08b854cbe6d127e3fe
Tilman Miraß <mail@tlmn.io>
2021-04-28 18:20:50 +0200

    load data

--- src/plugin/widget/src/components/calendar.jsx
+++ src/plugin/widget/src/components/calendar.jsx
@@ -1,12 +1,30 @@
+import React, { useEffect } from "react";
+import { loadEvents, loadTaxonomies, loadVenues } from "../lib/lib";
+
 import CalendarHeader from "./calendarHeader";
 import CalendarList from "./calendarList";
-import React from "react";
+import useCalendarContext from "../hooks/useCalendarContext";
 
-const Calendar = () => (
-  <div className="max-h-screen" style={{ margin: "0 auto" }}>
-    <CalendarHeader />
-    <CalendarList />
-  </div>
-);
+const Calendar = () => {
+  const { setData, data } = useCalendarContext();
+
+  const { eventData, venuesData } = data;
+  useEffect(() => {
+    loadTaxonomies(setData);
+    loadVenues(setData);
+  }, []);
+
+  useEffect(() => {
+    venuesData !== undefined && loadEvents(setData, venuesData);
+  }, [venuesData]);
+
+  return (
+    <div className="max-h-screen" style={{ margin: "0 auto" }}>
+      <CalendarHeader />
+      <CalendarList />
+      <pre>{JSON.stringify(eventData, null, 2)}</pre>
+    </div>
+  );
+};
 
 export default Calendar;
