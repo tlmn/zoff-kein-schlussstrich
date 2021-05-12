522090cc48866f2c25f685d2c225cad1483a34ca
Tilman Miraß <mail@tlmn.io>
2021-05-12 16:30:54 +0200

    add sub menu

--- src/theme/functions/func-menus.php
+++ src/theme/functions/func-menus.php
@@ -5,6 +5,7 @@ function register_menus()
     register_nav_menus(
         array(
             'navigation-menu' => __('Top Navigation Menü'),
+            'navigation-submenu' => __('Navigation Submenü'),
             'social-media' => __('Social Media'),
             'footer' => __('Footer')
         )
