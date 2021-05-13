ad05b8a82966cfa8abf4eb3f64657bfdcdc9a3f7
Tilman Miraß <mail@tlmn.io>
2021-05-13 11:44:40 +0200

    add padding

--- src/theme/template-parts/blocks/general/container-constraint.php
+++ src/theme/template-parts/blocks/general/container-constraint.php
@@ -9,7 +9,7 @@ $allowed_blocks = array('core/heading', 'core/paragraph', 'core/button', 'acf/im
 
 ?>
 <div class="w-full flex justify-center row-collapse">
-    <div class="container grid-6 md:grid-16 py-7 border-t-2 border-b-2 md:border-l-2 md:border-r-2">
+    <div class="container grid-6 md:grid-16 py-7 px-2 md:px-0 border-t-2 border-b-2 md:border-l-2 md:border-r-2">
         <div class="col-span-full md:col-span-10 md:col-start-4 ks-container--constraint">
             <InnerBlocks allowedBlocks="<?php echo esc_attr(wp_json_encode($allowed_blocks)); ?>" />
         </div>
