d5fae3c79e28596aa07f869bbf2064ad2a439370
Tilman Miraß <mail@tlmn.io>
2021-05-18 14:33:23 +0200

    locate json

--- src/theme/functions/func-acf.php
+++ src/theme/functions/func-acf.php
@@ -1,9 +1,12 @@
 <?php
 
+/**
+ * Read ACF fields from JSON
+ */
 function KS_acf_register_json_fields()
 {
     if (function_exists("acf_add_local_field_group")) {
-        $acf_json_data = file_get_contents(get_template_directory() + "/assets/acf/fields.json");
+        $acf_json_data = locate_template("/assets/acf/fields.json");
         $custom_fields = $acf_json_data ? json_decode(file_get_contents($acf_json_data), true) : array();
 
         foreach ($custom_fields as $custom_field) {
