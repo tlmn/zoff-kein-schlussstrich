715119c22805077acac549566d0965b1614d4874
Tilman Miraß <mail@tlmn.io>
2021-05-18 12:37:16 +0200

    add function for block styles

--- src/theme/functions/func-scripts.php
+++ src/theme/functions/func-scripts.php
@@ -9,3 +9,8 @@ function js_scripts_custom()
 {
     wp_enqueue_script('js-file', get_template_directory_uri() . '/assets/js/scripts/common.js', false);
 }
+
+function js_editor_block_styles()
+{
+    wp_enqueue_script('ks-editor', get_template_directory_uri() . '/assets/js/scripts/editor.js', array('wp-blocks', 'wp-dom'), true);
+}
