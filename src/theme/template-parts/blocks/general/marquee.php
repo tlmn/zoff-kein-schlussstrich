94380cb810456a317de17248da7d58ca2aba46b6
Tilman Miraß <mail@tlmn.io>
2021-05-19 10:57:12 +0200

    style

--- src/theme/template-parts/blocks/general/marquee.php
+++ src/theme/template-parts/blocks/general/marquee.php
@@ -9,8 +9,8 @@ $text = get_field("text") ?: "Laufband-Text";
 $bgColor = get_field("bgColor") ?: "white";
 
 ?>
-<div class="w-full flex justify-center ">
-    <div class="container py-7 border-2 bg-<?php echo $bgColor; ?>">
+<div class="w-full flex justify-center">
+    <div class="container py-7 border-l-2 border-r-2 bg-<?php echo $bgColor; ?> shadow--bottom">
         <span class="w-full h-full block h7 overflow-hidden" id="marquee-cities-wrapper">
             <?php echo $text; ?>
         </span>
