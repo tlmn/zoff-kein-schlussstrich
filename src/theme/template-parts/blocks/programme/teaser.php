ea1453a9c96c44a3a82ddf916365310f71fcb3d9
Tilman Miraß <mail@tlmn.io>
2021-05-20 17:14:57 +0200

    remove classes

--- src/theme/template-parts/blocks/programme/teaser.php
+++ src/theme/template-parts/blocks/programme/teaser.php
@@ -13,11 +13,9 @@ $template = array(
     array('core/heading', array(
         'content'   => '18 Tage 150 Veranstaltungen 14 Städte',
         'level'     => 2,
-        'className' => 'block md:mb-4 font-medium text-2xl md:text-5xl md:leading-tight'
     )),
     array('core/paragraph', array(
         'content' => 'Bundesweites & interdisziplinäres Theaterprojekt „Kein Schlussstrich!“ thematisiert künstlerisch die Taten und Hintergründe des NSU künstlerisch zu thematisieren. Mit dem Vorhaben sollen die Perspektiven der Familien der Opfer und der migrantischen Communities in den Fokus der Öffentlichkeit gebracht werden.',
-        'className' => 'block font-medium text-m md:text-lg leading-wider'
     )),
     array('core/button', array(
         'content' => 'About',
@@ -28,30 +26,32 @@ $template = array(
 ?>
 
 <div class="border-t-2 border-b-2 row-collapse flex justify-center">
-    <div class="container grid-6 md:grid-16 border-l-2 border-r-2">
-        <div class="md:border-r-2 col-span-full md:col-span-1 flex gap-collapse--right h-full items-center justify-center md:relative">
+    <div class="container grid-6 md:grid-16 md:border-l-2 md:border-r-2">
+        <div class="md:border-r-2 col-span-full md:col-span-1 flex md:gap-collapse--right h-full items-center justify-center md:relative">
             <span class="font-sans font-medium md:absolute md:rotate--270 text-3xl md:text-xl py-3 md:py-0 leading-snug md:max-w-max md:whitespace-nowrap">
                 Programm
             </span>
         </div>
-        <div class="col-span-full md:col-span-4 flex flex-col border-r-2">
-            <div class="flex-1 w-full">
+        <div class="col-span-full md:col-span-4 flex flex-col md:border-r-2">
+            <div class="flex-1 w-full shadow--bottom" style="z-index: 100">
                 <img srcset="<?php echo wp_get_attachment_image_srcset($featureImage['ID']); ?>" alt="<?php echo $featureImage['alt']; ?>" class="w-full h-full object-cover" />
             </div>
             <div class="self-end w-full flex flex-col">
                 <?php
+                $i = 1;
                 foreach ($programmeLinks as $programmeLink) {
                 ?>
-                    <a href="<?php echo get_permalink($programmeLink['link']->ID); ?>" class="w-full px-2 py-1 text-2xl text-black leading-wider font-normal shadow--bottom-right z-20 bg-<?php echo $programmeLink['bgColor'] ?>">
+                    <a href="<?php echo get_permalink($programmeLink['link']->ID); ?>" class="no-underline w-full px-2 py-1 text-2xl text-black leading-wider font-normal shadow--bottom z-20 bg-<?php echo $programmeLink['bgColor'] ?>" style="z-index: <?php echo 100 - ($i * 10); ?>">
                         <?php echo $programmeLink['link']->post_title ?>
                     </a>
                 <?php
+                    $i++;
                 }
                 ?>
             </div>
         </div>
 
-        <div class="col-span-full md:col-span-9 py-7 md:p-7">
+        <div class="col-span-full md:col-span-9 py-7 md:p-7 px-2 md:px-0 ">
             <InnerBlocks allowedBlocks="<?php echo esc_attr(wp_json_encode($allowed_blocks)); ?>" template="<?php echo esc_attr(wp_json_encode($template)); ?>" templateLock="all" />
         </div>
     </div>
