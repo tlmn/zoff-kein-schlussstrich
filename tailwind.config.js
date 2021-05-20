1a46ce601e32751a352925e117f0440a62357143
Tilman Miraß <mail@tlmn.io>
2021-05-20 17:13:04 +0200

    add grid 15

--- tailwind.config.js
+++ tailwind.config.js
@@ -16,6 +16,7 @@ module.exports = {
       gridTemplateColumns: {
         6: "repeat(6, minmax(0, 1fr))",
         14: "repeat(14, minmax(0, 1fr))",
+        15: "repeat(15, minmax(0, 1fr))",
         16: "repeat(16, minmax(0, 1fr))",
       },
       gridColumn: {
