b702778ad86102731de4a8ebf7b758f830254e9f
Tilman Miraß <mail@tlmn.io>
2021-05-20 17:13:17 +0200

    create footer

--- src/theme/footer.php
+++ src/theme/footer.php
@@ -1,5 +1,13 @@
-<footer>
-    
+<div class="footer">
+    <div class="container flex justify-end">
+        <?php
+        wp_nav_menu(array('theme_location' => 'footer', 'depth' => 1, 'menu_class' => 'is-style-text-running', 'container_id' => 'footer-menu'));
+        ?>
+    </div>
+</div>
+
+
+
 <?php wp_footer(); ?>
 </body>
 
