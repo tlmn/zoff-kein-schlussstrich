842fd33198a7a23254311716c64fd3a1d193b159
Tilman Miraß <mail@tlmn.io>
2021-06-29 15:11:12 +0200

    add wrapper class

--- src/plugin/widget/src/components/filters/cities.jsx
+++ src/plugin/widget/src/components/filters/cities.jsx
@@ -8,36 +8,38 @@ const CitiesFilter = () => {
     taxonomies: { cities },
     filters: { city },
   } = data;
-  
+
   return (
-    <select
-      onChange={(event) => {
-        setData((prev) => ({
-          ...prev,
-          filters: {
-            ...prev.filters,
-            city:
-              event.target.value === "alle Orte" ? null : event.target.value,
-            currentDate: 0,
-          },
-        }));
-      }}
-      className="my-1 mx-2 bg-lightGray rounded-full p-1 text-black calendar__select"
-      style={{ outline: 0 }}
-    >
-      {cities.map(
-        (item, index) =>
-          item !== "" && (
-            <option
-              value={item}
-              selected={item === "alle Orte" && city === null}
-              key={index}
-            >
-              {item}
-            </option>
-          )
-      )}
-    </select>
+    <div className="select__wrapper">
+      <select
+        onChange={(event) => {
+          setData((prev) => ({
+            ...prev,
+            filters: {
+              ...prev.filters,
+              city:
+                event.target.value === "alle Orte" ? null : event.target.value,
+              currentDate: 0,
+            },
+          }));
+        }}
+        className="text-black text-center"
+        style={{ outline: 0 }}
+      >
+        {cities.map(
+          (item, index) =>
+            item !== "" && (
+              <option
+                value={item}
+                selected={item === "alle Orte" && city === null}
+                key={index}
+              >
+                {item}
+              </option>
+            )
+        )}
+      </select>
+    </div>
   );
 };
 
