342bd81f529fbf0bd2a9b43c238369ce9e17f573
Tilman Miraß <mail@tlmn.io>
2021-06-29 15:11:33 +0200

    add margin to range

--- src/plugin/widget/src/components/navigation/navigationRange.jsx
+++ src/plugin/widget/src/components/navigation/navigationRange.jsx
@@ -20,49 +20,51 @@ const NavigationRange = () => {
           {filteredDates[0]}
         </span>
       )}
-
-      <Range
-        step={1}
-        min={0}
-        max={filteredDates.length - 1}
-        values={[currentDate]}
-        onChange={(value) => {
-          filteredDates.length > 1 &&
-            setData((prev) => ({
-              ...prev,
-              filters: {
-                ...prev.filters,
-                currentDate: value,
-              },
-            }));
-        }}
-        renderTrack={({ props, children }) => (
-          <div
-            {...props}
-            style={{
-              ...props.style,
-              height: "2px",
-              width: "100%",
-              backgroundColor: "#fff",
-            }}
-          >
-            {children}
-          </div>
-        )}
-        renderThumb={({ props }) => (
-          <div
-            {...props}
-            style={{
-              ...props.style,
-              height: "20px",
-              width: "20px",
-              backgroundColor: "#0094FF",
-              borderRadius: "9999px",
-              outline: 0,
-            }}
-          />
-        )}
-      />
+      
+      <div className="md:mx-3 w-full">
+        <Range
+          step={1}
+          min={0}
+          max={filteredDates.length - 1}
+          values={[currentDate]}
+          onChange={(value) => {
+            filteredDates.length > 1 &&
+              setData((prev) => ({
+                ...prev,
+                filters: {
+                  ...prev.filters,
+                  currentDate: value,
+                },
+              }));
+          }}
+          renderTrack={({ props, children }) => (
+            <div
+              {...props}
+              style={{
+                ...props.style,
+                height: "2px",
+                width: "100%",
+                backgroundColor: "#fff",
+              }}
+            >
+              {children}
+            </div>
+          )}
+          renderThumb={({ props }) => (
+            <div
+              {...props}
+              style={{
+                ...props.style,
+                height: "20px",
+                width: "20px",
+                backgroundColor: "#0094FF",
+                borderRadius: "9999px",
+                outline: 0,
+              }}
+            />
+          )}
+        />
+      </div>
 
       {filteredDates.length > 1 && (
         <span className="hidden md:block ml-3 font-sans font-normal text-m text-white">
