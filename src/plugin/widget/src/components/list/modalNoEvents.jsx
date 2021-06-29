c418b91ef7614e68805f8cff162467645090e848
Tilman Miraß <mail@tlmn.io>
2021-06-29 15:11:24 +0200

    style modal

--- src/plugin/widget/src/components/list/modalNoEvents.jsx
+++ src/plugin/widget/src/components/list/modalNoEvents.jsx
@@ -1,12 +1,22 @@
 import React from "react";
 import ResetButton from "../filters/resetButton";
+import useCalendarContext from "../../hooks/useCalendarContext";
 
 const ModalNoEvents = () => {
+  const { setData } = useCalendarContext();
+
   return (
-    <div className="fixed w-full flex justify-center border-b-2 bg-black">
+    <div className="fixed z-50 w-full h-full flex items-center border-b-2 bg-black bg-opacity-50 top-0 left-0" style={{ backdropFilter: "blur(3px)" }} onClick={() => setData((prev) => ({
+      ...prev,
+      filters: {
+        ...prev.filters,
+        division: null,
+        city: null,
+      },
+    }))}>
       <div className="container grid-6 md:grid-16">
-        <div className="col-span-full py-4 px-2 flex flex-col items-center">
-          <span className="text-white text-lg text-center font-sans leading-snug">
+        <div className="col-span-full md:col-span-8 md:col-start-5 bg-black p-5 flex flex-col items-center rounded-md shadow--bottom-right">
+          <span className="text-white text-lg text-center font-sans leading-snug mb-3">
             Keine Veranstaltungen mit diesen Filtereinstellungen.
           </span>
           <ResetButton />
