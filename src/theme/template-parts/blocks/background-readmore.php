9038fd461668e8771c69916d640d44ef58502747
Tilman Miraß <mail@tlmn.io>
2021-06-30 12:12:40 +0200

    fix styling in backend

--- src/theme/template-parts/blocks/background-readmore.php
+++ src/theme/template-parts/blocks/background-readmore.php
@@ -15,14 +15,14 @@ $template = array(
 
 ?>
 
-<div class="container grid-6 md:grid-16 px-2 md:px-0 bg-white md:border-l-2 md:border-r-2 border-black">
+<div class="container grid-6 md:grid-16 px-2 md:px-0 bg-white <?php if (!$is_preview) { ?>md:border-l-2 md:border-r-2 border-black<?php } ?>">
     <div class="col-span-full md:col-span-11 md:col-start-4">
         <div class="z-50 sticky" style="top: 50px">
             <button type="button" class="wp-block-button bg-white text-black readmore-button" style="transform:translateY(-50%)">
                 Mehr lesen
             </button>
         </div>
-        <div class="py-5 md:py-6" style="display: none">
+        <div class="py-5 md:py-6" <?php if (!$is_preview) { ?>style="display: none"<?php } ?>>
             <InnerBlocks allowedBlocks="<?php echo esc_attr(wp_json_encode($allowed_blocks)); ?>" template="<?php echo esc_attr(wp_json_encode($template)); ?>" />
         </div>
     </div>
