d23126cc813b895e817bd87c3ec74d5e45c650d5
Tilman Miraß <mail@tlmn.io>
2021-07-01 16:00:55 +0200

    reduce icon width

--- src/plugin/widget/src/components/calendarHeader.jsx
+++ src/plugin/widget/src/components/calendarHeader.jsx
@@ -84,7 +84,7 @@ export const CalendarHeader = () => {
         </div>
         <div className="px-2 col-span-full flex justify-center">
           <div className="flex items-center gap-3">
-            <FilterIcon style={{ width: "90px" }} />
+            <FilterIcon style={{ width: "30px" }} />
             <CitiesFilter />
             <DivisionsFilter />
             <ResetButton />
