87aa84e4f489d1354c09f705092df96542c2b729
Tilman Miraß <mail@tlmn.io>
2021-07-01 16:03:31 +0200

    add margin

--- src/plugin/widget/src/components/list/modalNoEvents.jsx
+++ src/plugin/widget/src/components/list/modalNoEvents.jsx
@@ -15,7 +15,7 @@ const ModalNoEvents = () => {
       },
     }))}>
       <div className="container grid-6 md:grid-16">
-        <div className="col-span-full md:col-span-8 md:col-start-5 bg-black p-5 flex flex-col items-center rounded-md shadow--bottom-right">
+        <div className="col-span-full md:col-span-8 md:col-start-5 bg-black p-5 mx-5 flex flex-col items-center rounded-md shadow--bottom-right">
           <span className="text-white text-lg text-center font-sans leading-snug mb-3">
             Keine Veranstaltungen mit diesen Filtereinstellungen.
           </span>
