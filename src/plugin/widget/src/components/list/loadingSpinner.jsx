f74b3d9475d9e867338ed0fd0d56023701b699a0
Tilman Miraß <mail@tlmn.io>
2021-07-01 16:22:51 +0200

    make loading spinner smaller

--- src/plugin/widget/src/components/list/loadingSpinner.jsx
+++ src/plugin/widget/src/components/list/loadingSpinner.jsx
@@ -2,8 +2,8 @@ import KSLogo from "../../assets/icons/ks-logo";
 import React from "react";
 
 const LoadingSpinner = () => (
-  <div className="w-full flex justify-center border-b-2 bg-black">
-    <div className="container grid-6 md:grid-16">
+  <div className="w-full flex justify-center border-b-2">
+    <div className="container grid-6 md:grid-16 bg-black">
       <div className="col-span-full py-4 px-2 flex flex-col items-center">
         <span className="text-white text-lg font-sans">Lade Kalender</span>
         <KSLogo className="calendar__loading-spinner" />
