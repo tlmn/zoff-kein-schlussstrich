4b52b14c5d2d9723126591bb45ffc70a02a5f850
Tilman Miraß <mail@tlmn.io>
2021-07-01 15:23:57 +0200

    use luxon

--- src/plugin/widget/src/lib/lib.js
+++ src/plugin/widget/src/lib/lib.js
@@ -1,32 +1,19 @@
 import { concat, remove } from "lodash";
 
+import { DateTime } from "luxon";
 import jsonata from "jsonata";
 
-export const generateLink = (link, datetime) => {
-  let date = new Date(datetime);
-
-  return `${link}?date=${pad(date.getUTCDate(), 2)}${pad(
-    date.getMonth() + 1,
-    2
-  )}${date.getFullYear()}&time=${pad(date.getHours(), 2)}${pad(
-    date.getMinutes(),
-    2
-  )}`;
+export const generateLink = (link, luxonTimestamp) => {
+  return `${link}?date=${luxonTimestamp.toFormat("ddMMyyyy")}&time=${luxonTimestamp.toFormat("HHmm")}`;
 };
 
 export const convertToDate = (datetime) => {
   let date = new Date(datetime);
-  return `${date.getUTCDate()}.${
-    date.getMonth() + 1
-  }.${date.getFullYear()} ${pad(date.getHours(), 2)}:${pad(
-    date.getMinutes(),
-    2
-  )} Uhr`;
-};
-
-export const getTime = (datetime) => {
-  let date = new Date(datetime);
-  return `${pad(date.getHours(), 2)}:${pad(date.getMinutes(), 2)}`;
+  return `${date.getUTCDate()}.${date.getMonth() + 1
+    }.${date.getFullYear()} ${pad(date.getHours(), 2)}:${pad(
+      date.getMinutes(),
+      2
+    )} Uhr`;
 };
 
 export const pad = (num, size) => {
@@ -59,17 +46,6 @@ export const getWeekDay = (dateLong) => {
   return weekDays[date.getDay()];
 };
 
-const getShortDate = (datetime) => {
-  let date = new Date(datetime);
-
-  return `${date.getUTCDate()}.${date.getMonth() + 1}.`;
-};
-
-const getLongDate = (datetime) => {
-  let date = new Date(datetime);
-  return `${date.getUTCDate()}.${date.getMonth() + 1}.${date.getFullYear()}`;
-};
-
 export const parseEvents = (eventsRaw, venuesData, data) => {
   const {
     taxonomies: { divisions },
@@ -83,32 +59,35 @@ export const parseEvents = (eventsRaw, venuesData, data) => {
         item.acf.meta.occurrences !== undefined &&
         item.acf.meta.occurrences.length > 0 &&
         item.acf.meta.occurrences.map(
-          (occ) =>
-            occ.show_in_calendar === true &&
-            eventsParsed.push({
-              alarm: occ.alarm,
-              dateLong: getLongDate(new Date(occ.timestamp)),
-              dateShort: getShortDate(new Date(occ.timestamp)),
-              datetime: convertToDate(new Date(occ.timestamp)),
-              division: item.division.length !== 0 ? item.division[0] : 0,
-              duration: item.duration,
-              feature_image: {
-                sizes: item.acf.meta.feature_image.image.sizes,
-                alt: item.acf.meta.feature_image.image.alt,
-              },
-              link: generateLink(item.link, new Date(occ.timestamp).toJSON()),
-              short_description: item.acf.meta.short_description,
-              tags:
-                item.division.length > 0
-                  ? concat(
+          (occ) => {
+            const { timestamp } = occ;
+            const luxonTimestamp = DateTime.fromFormat(timestamp, "yyyy/MM/dd HH:mm:ss");
+            console.log(luxonTimestamp)
+            return occ.show_in_calendar === true &&
+              eventsParsed.push({
+                alarm: occ.alarm,
+                dateLong: luxonTimestamp.toFormat("dd.MM.yyyy"),
+                dateShort: luxonTimestamp.toFormat("dd.MM."),
+                datetime: luxonTimestamp.toFormat("dd.MM.yyyy HH:mm"),
+                division: item.division.length !== 0 ? item.division[0] : 0,
+                duration: item.duration,
+                feature_image: {
+                  sizes: item.acf.meta.feature_image.image.sizes,
+                  alt: item.acf.meta.feature_image.image.alt,
+                },
+                link: generateLink(item.link, luxonTimestamp),
+                short_description: item.acf.meta.short_description,
+                tags:
+                  item.division.length > 0
+                    ? concat(
                       divisions.divisions !== undefined &&
-                        divisions.divisions.filter(
-                          (division) => division.id === item.division[0]
-                        )[0].name,
+                      divisions.divisions.filter(
+                        (division) => division.id === item.division[0]
+                      )[0].name,
                       occ.venue !== undefined && occ.venue.length > 0
                         ? venuesData[occ.venue[0].ID.toString()] !== undefined
                           ? venuesData[occ.venue[0].ID.toString()].acf.address
-                              .city
+                            .city
                           : ""
                         : "",
                       occ.venue !== undefined && occ.venue.length > 0
@@ -117,27 +96,28 @@ export const parseEvents = (eventsRaw, venuesData, data) => {
                           : ""
                         : "",
                       occ.labels !== undefined &&
-                        occ.labels.length > 0 &&
-                        occ.labels
+                      occ.labels.length > 0 &&
+                      occ.labels
                     )
-                  : [],
-              ticketlink: occ.ticketlink,
-              time: getTime(new Date(occ.timestamp)),
-              timestamp: new Date(occ.timestamp).getTime(),
-              title: item.title.rendered,
-              city:
-                occ.venue !== undefined && occ.venue.length > 0
-                  ? venuesData[occ.venue[0].ID.toString()] !== undefined
-                    ? venuesData[occ.venue[0].ID.toString()].acf.address.city
-                    : ""
-                  : null,
-              venue:
-                occ.venue !== undefined && occ.venue.length > 0
-                  ? venuesData[occ.venue[0].ID.toString()] !== undefined
-                    ? venuesData[occ.venue[0].ID.toString()].acf
-                    : ""
-                  : "",
-            })
+                    : [],
+                ticketlink: occ.ticketlink,
+                time: luxonTimestamp.toFormat("HH:mm"),
+                timestamp: luxonTimestamp.toFormat("X"),
+                title: item.title.rendered,
+                city:
+                  occ.venue !== undefined && occ.venue.length > 0
+                    ? venuesData[occ.venue[0].ID.toString()] !== undefined
+                      ? venuesData[occ.venue[0].ID.toString()].acf.address.city
+                      : ""
+                    : null,
+                venue:
+                  occ.venue !== undefined && occ.venue.length > 0
+                    ? venuesData[occ.venue[0].ID.toString()] !== undefined
+                      ? venuesData[occ.venue[0].ID.toString()].acf
+                      : ""
+                    : "",
+              })
+          }
         )
     );
 
@@ -158,11 +138,10 @@ export const generateSrcSet = (sizes) => {
 export const loadEvents = async (setData, venuesData, data) => {
   const response = await fetch(
     typeof window !== undefined &&
-      `${window.location.protocol}//${window.location.hostname}${
-        window.location.port !== "" && window.location.port !== "443"
-          ? `:8000`
-          : ``
-      }/wp-json/wp/v2/event?_fields=acf,link,title,division,labels
+    `${window.location.protocol}//${window.location.hostname}${window.location.port !== "" && window.location.port !== "443"
+      ? `:8000`
+      : ``
+    }/wp-json/wp/v2/event?_fields=acf,link,title,division,labels
       `
   );
 
@@ -195,11 +174,10 @@ export const loadEvents = async (setData, venuesData, data) => {
 export const loadVenues = async (setData) => {
   const response = await fetch(
     typeof window !== undefined &&
-      `${window.location.protocol}//${window.location.hostname}${
-        window.location.port !== "" && window.location.port !== "443"
-          ? `:8000`
-          : ``
-      }/wp-json/wp/v2/eventLocation?_fields=acf,id`
+    `${window.location.protocol}//${window.location.hostname}${window.location.port !== "" && window.location.port !== "443"
+      ? `:8000`
+      : ``
+    }/wp-json/wp/v2/eventLocation?_fields=acf,id`
   );
 
   if (!response.ok) {
@@ -220,8 +198,8 @@ export const scrollToDate = (filteredDates, currentDate, listRef) => {
   const offsetTopID =
     filteredDates[currentDate] !== undefined
       ? [...listRef.current.childNodes].filter((node) => {
-          return node.id === filteredDates[currentDate].replace(/\./g, "");
-        })[0].offsetTop
+        return node.id === filteredDates[currentDate].replace(/\./g, "");
+      })[0].offsetTop
       : 0;
 
   const offsetTopList = listRef.current.offsetTop;
@@ -232,11 +210,10 @@ export const scrollToDate = (filteredDates, currentDate, listRef) => {
 export const loadTaxonomies = async (setData) => {
   const response = await fetch(
     typeof window !== undefined &&
-      `${window.location.protocol}//${window.location.hostname}${
-        window.location.port !== "" && window.location.port !== "443"
-          ? `:8000`
-          : ``
-      }/wp-json/wp/v2/division?_fields=id,name`
+    `${window.location.protocol}//${window.location.hostname}${window.location.port !== "" && window.location.port !== "443"
+      ? `:8000`
+      : ``
+    }/wp-json/wp/v2/division?_fields=id,name`
   );
 
   if (!response.ok) {
@@ -271,38 +248,35 @@ export const filterEvents = (data, setData) => {
     window.history.replaceState(
       null,
       "Veranstaltungskalender",
-      `${currentDate || (division !== null ? `?` : ``)}${
-        currentDate !== null ? `date=${currentDate}` : ``
-      }${
-        division !== null
-          ? `${currentDate !== null ? `&` : ``}saeule=${division}`
-          : ``
+      `${currentDate || (division !== null ? `?` : ``)}${currentDate !== null ? `date=${currentDate}` : ``
+      }${division !== null
+        ? `${currentDate !== null ? `&` : ``}saeule=${division}`
+        : ``
       }`
     );
 
   const filterExpression = jsonata(
     division !== null || city !== null
       ? `($filter(*, function($v) {
-         ${division !== null ? `$v."division" = ${division}` : ``} ${
-          division !== null && city !== null ? `and` : ``
-        } ${city !== null ? `$v."city" = "${city}"` : ``}
+         ${division !== null ? `$v."division" = ${division}` : ``} ${division !== null && city !== null ? `and` : ``
+      } ${city !== null ? `$v."city" = "${city}"` : ``}
        }))^(timestamp){dateLong:[$]}`
       : `$`
   );
 
   division !== "" && city !== ""
     ? setData((prev) => ({
-        ...prev,
-        eventData: {
-          ...prev.eventData,
-          filteredEvents: filterExpression.evaluate(initialEvents),
-        },
-      }))
+      ...prev,
+      eventData: {
+        ...prev.eventData,
+        filteredEvents: filterExpression.evaluate(initialEvents),
+      },
+    }))
     : setData((prev) => ({
-        ...prev,
-        eventData: {
-          ...prev.eventData,
-          filteredEvents: prev.initialEvents,
-        },
-      }));
+      ...prev,
+      eventData: {
+        ...prev.eventData,
+        filteredEvents: prev.initialEvents,
+      },
+    }));
 };
