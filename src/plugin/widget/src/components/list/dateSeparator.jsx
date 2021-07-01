b8525949e058cdcd040a2eba62ed9ddbed4a0283
Tilman Miraß <mail@tlmn.io>
2021-07-01 15:48:31 +0200

    add weekday

--- src/plugin/widget/src/components/list/dateSeparator.jsx
+++ src/plugin/widget/src/components/list/dateSeparator.jsx
@@ -1,18 +1,28 @@
+import { DateTime } from "luxon";
 import PropTypes from "prop-types";
 import React from "react";
 
 const DateSeparator = ({ date }) => {
+  const luxon = DateTime.fromFormat(date, "dd.MM.yyyy").setLocale("de");
+  console.log(luxon)
   return (
     <div className="container grid-16 bg-white">
-      <div className="col-span-full flex justify-center bg-black">
+      <div className="col-span-full flex justify-center bg-black text-white">
+        <span
+          className="hidden md:inline-block font-sans text-white text-base leading-snug"
+          style={{ transform: "rotate(270deg)" }}
+        >
+          {luxon.toFormat("cccc")}
+        </span>
+
         <h2
-          className="font-sans font-normal text-6xl md:text-7xl text-white z-10"
+          className="font-sans font-normal text-6xl md:text-7xl z-10"
           style={{
             marginTop: "-2px",
             lineHeight: 0.7,
           }}
         >
-          {date.slice(0, -4)}
+          {luxon.toFormat("dd.MM.")}
         </h2>
       </div>
     </div>
