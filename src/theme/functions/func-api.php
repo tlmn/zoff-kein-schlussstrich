a829aacdc508537ad3d14592428851551f99f993
Tilman Miraß <mail@tlmn.io>
2021-07-06 13:47:59 +0200

    increase api limit

--- src/theme/functions/func-api.php
+++ src/theme/functions/func-api.php
@@ -3,3 +3,12 @@ function add_cors_http_header()
 {
     header("Access-Control-Allow-Origin: *");
 }
+
+function api_override_per_page($params)
+{
+    if (isset($params) and isset($params['posts_per_page'])) {
+        $params['posts_per_page'] = 500;
+    }
+
+    return $params;
+}
