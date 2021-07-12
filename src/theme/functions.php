d31b41963b2934e289bb60f96f6d35d962254ed7
Tilman Miraß <mail@tlmn.io>
2021-07-12 13:43:59 +0200

    remove api limit

--- src/theme/functions.php
+++ src/theme/functions.php
@@ -35,5 +35,3 @@ add_action('enqueue_block_editor_assets', 'js_editor_block_styles');
 add_action('init', 'remove_comment_support', 100);
 add_action('admin_menu', 'remove_admin_menus');
 add_action('wp_before_admin_bar_render', 'admin_bar_render');
-
-add_action('rest_event_query', 'api_override_per_page');
