ace5848c630e0acbb60bfe000030ff093a7b6eec
tlmn <mail@tlmn.io>
2021-08-16 17:15:01 +0200

    set current language

--- src/plugin/widget/src/App.js
+++ src/plugin/widget/src/App.js
@@ -2,12 +2,14 @@ import "./assets/style.css";
 import "./assets/select.css";
 
 import React, { useState } from "react";
+import Cookies from "js-cookie";
 
 import Calendar from "./components/calendar";
 import { Provider as CalendarContextProvider } from "./hooks/useCalendarContext";
 
 const App = () => {
   const [data, setData] = useState({
+    currentLanguage: Cookies.get("wp-wpml_current_language"),
     eventData: {
       initialEvents: [],
       filteredEvents: [],
