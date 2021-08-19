8a5644cf23686e4f837f8537d2d712c76818e9b7
tlmn <mail@tlmn.io>
2021-08-19 08:56:23 +0200

    use 3 chars for short weekday

--- src/plugin/widget/src/components/list/dateSeparator.jsx
+++ src/plugin/widget/src/components/list/dateSeparator.jsx
@@ -1,9 +1,12 @@
 import { DateTime } from "luxon";
 import PropTypes from "prop-types";
 import React from "react";
+import useCalendarContext from "../../hooks/useCalendarContext";
 import { getWeekDay } from "../../lib/lib";
 
 const DateSeparator = ({ date }) => {
+  const { data: currentLocale } = useCalendarContext;
+
   const luxon = DateTime.fromFormat(date, "dd.MM.yyyy").setLocale("de");
   return (
     <div className="container grid-16 bg-white">
@@ -12,7 +15,7 @@ const DateSeparator = ({ date }) => {
           className="hidden md:inline-block font-sans uppercase text-white text-m leading-snug"
           style={{ transform: "rotate(270deg)" }}
         >
-          {getWeekDay(date).substring(0, 2)}
+          {getWeekDay(date).substring(0, currentLocale !== "en" ? 2 : 3)}
         </span>
 
         <span className="block md:hidden font-sans text-white text-base leading-snug my-2">
