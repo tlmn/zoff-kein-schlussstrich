e874f406d524be9b73ef784fcd62f2907e26736a
tlmn <mail@tlmn.io>
2021-10-12 20:09:04 +0200

    disable

--- src/theme/footer.php
+++ src/theme/footer.php
@@ -1,12 +1,16 @@
 <div class="footer">
     <div class="container flex flex-col justify-center md:flex-row md:items-center md:justify-between">
-        <?php
-        wp_nav_menu(array('theme_location' => 'footer', 'depth' => 1, 'menu_class' => 'is-style-text-running', 'container_id' => 'footer-menu'));
-        ?>
+        <?php wp_nav_menu([
+            'theme_location' => 'footer',
+            'depth' => 1,
+            'menu_class' => 'is-style-text-running',
+            'container_id' => 'footer-menu',
+        ]); ?>
         <div class="my-4">
-            <?php
-            wp_nav_menu(array('theme_location' => 'social-media', 'depth' => 1));
-            ?>
+            <?php wp_nav_menu([
+                'theme_location' => 'social-media',
+                'depth' => 1,
+            ]); ?>
         </div>
     </div>
 </div>
@@ -14,10 +18,8 @@
 <?php wp_footer(); ?>
 </body>
 
-<div id="language-menu">
-						<?php 
-						print_r(icl_get_languages());
-						?>
-					</div>
+<!-- <div id="language-menu">
+						<?php print_r(icl_get_languages()); ?>
+					</div> -->
 
 </html>
